65d9828e50b4c45022d7f2f61746a00176922ecc
jks2024 <jks2024@gmail.com>
2025-10-22 15:16:58 +0900

    Initial commit

---
+++ src/main/java/com/human/jdbc251022/model/Member.java
@@ -0,0 +1,13 @@
+package com.human.jdbc251022.model;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+@AllArgsConstructor @NoArgsConstructor
+public class Member {
+    private String email;
+    private String pwd;
+    private String name;
+    public LocalDateTime regDate;
+}
